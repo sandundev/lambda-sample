227b73427b6ae19753c64f287b47e3c3f270276b
sandundev <sandun.lewke@gmail.com>
2020-11-25 10:38:22 +0000

    Add files via upload

--- src/handlers/get-all-items.js
+++ src/handlers/get-all-items.js
@@ -1,21 +1,35 @@
 // Create clients and set shared const values outside of the handler.
 
-// Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
-
-// Create a DocumentClient that represents the query to add an item
-const dynamodb = require('aws-sdk/clients/dynamodb');
-const docClient = new dynamodb.DocumentClient();
+const AWS = require('aws-sdk');
+const ssm = new AWS.SSM({region: 'eu-west-1'});
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
 exports.getAllItemsHandler = async (event) => {
 
+    const param = await getParameter({
+        Name: 'SCP_BASE_URL'
+    });
+
+    console.log('base url paramVal3: ', param);
+
     const response = {
         statusCode: 200,
-        body: "response!"
+        body: "response! url: " + param.Parameter.Value
     };
 
     return response;
 }
+
+function getParameter(param) {
+    return new Promise(function (success, reject) {
+        ssm.getParameter(param, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                success(data);
+            }
+        });
+    });
+};
